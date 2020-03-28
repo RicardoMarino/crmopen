1ed8be9ee1a9c3bdea812e7228455777794742be
Ricardo Marino <ricardomarino@live.com>
2020-03-28 15:33:30 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/crmopen/app/CrmOpenApplication.java
@@ -0,0 +1,13 @@
+package br.com.crmopen.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmOpenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmOpenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/crmopen/app/CrmOpenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crmopen.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmOpenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
